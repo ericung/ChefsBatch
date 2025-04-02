69e378ad8991ab5c4a4983287d35ec70a748916a
ericung <ungericw@tutanota.com>
2025-04-02 10:44:53 -0500

    04-02-2-2025 CustomerModel and DishModel appropriation

--- ChefsBatch/Models/CustomerModel.cs
+++ ChefsBatch/Models/CustomerModel.cs
@@ -1,14 +1,23 @@
 ﻿using System.Collections.Generic;
 
+using System.Collections.Generic;
+
 namespace ChefsBatch.Models
 {
+    // Customer data model
     public class CustomerModel
     {
-        public int Id { get; set; }
+        public int CustomerId { get; set; }
         public string Name { get; set; }
+        public string FullName { get; set; }
         public string Email { get; set; }
+        public List<DishModel> PurchasedDishes { get; set; } // Dishes purchased by the customer
+        public List<ChefModel> FavoriteChefs { get; set; } // Chefs followed by the customer
+        public string DietaryPreferences { get; set; } // e.g., Vegan, Gluten-Free
+        public string SubscriptionPlan { get; set; } // e.g., Basic, Premium
         public CartModel Cart { get; set; } = new CartModel(); // Initialize a new CartModel instance
     }
+
 
     public class CartModel
     {
